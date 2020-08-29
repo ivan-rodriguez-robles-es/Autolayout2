f8cd6a90423bfd4556604296e316e3adc7d21ad8
ivan-rodriguez-robles-es <ivan.rodriguez.robles.es@gmail.com>
2020-08-29 15:50:05 +0200

    Initial Commit

---
+++ Autolayout2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autolayout2
+//
+//  Created by ivk on 29/08/2020.
+//  Copyright © 2020 irr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
